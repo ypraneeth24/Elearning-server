e7d49faca0b020fe158e23803162b07bbc292469
ypraneeth24 <yeshaboinapraneeth@gmail.com>
2025-01-07 18:06:44 +0530

    a small update in lecture model in the ref

--- models/lecture.js
+++ models/lecture.js
@@ -15,7 +15,7 @@ const schema = new mongoose.Schema({
     },
     course: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "course",
+        ref: "Courses",
         required:true,
     },
     createdAt:{
